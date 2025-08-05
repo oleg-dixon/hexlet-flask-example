2faa0e1ad3fc540f7999f446bed92514325c041a
oleg-dixon <olegdixon5973@gmail.com>
2025-08-05 17:31:55 +0500

    Update and add files

--- example.py
+++ example.py
@@ -1,53 +1,115 @@
-from flask import Flask, render_template, request
-import logging
-# Это callable WSGI-приложение
+from flask import (
+    get_flashed_messages,
+    flash,
+    Flask,
+    redirect,
+    render_template,
+    request,
+    url_for
+)
+from user_repository import UserRepository
+
 app = Flask(__name__)
-
-
-users = [
-    {"id": 1, "name": "mike"},
-    {"id": 2, "name": "mishel"},
-    {"id": 3, "name": "adel"},
-    {"id": 4, "name": "keks"},
-    {"id": 5, "name": "kamila"},
-]
-
-
-app.logger.setLevel(logging.DEBUG)
+app.secret_key = "secret_key"
 
 
 @app.route('/')
 def index():
-    app.logger.info(f'Получен запрос к главной странице')
-    return 'Добро пожаловать!'
-
-@app.route('/error')
-def error():
-    app.logger.info(f'Произошла ошибка')
-    return 'Ошибка!', 500
-
-@app.route("/")
-def hello_world():
-    return "Welcome to Flask!"
+    return 'Welcome to Flask!'
 
 
 @app.route('/users/')
-def get_users():
+def users_get():
+    messages = get_flashed_messages(with_categories=True)
     term = request.args.get('term', '')
-    print(users)
+    repo = UserRepository()
+    users = repo.get_content()
     filtered_users = [user for user in users if term in user['name']]
     return render_template(
         'users/index.html',
         users=filtered_users,
         search=term,
+        messages=messages
     )
 
 
-@app.post("/users")
+@app.post('/users')
 def users_post():
-    return "POST /users"
+    user_data = request.form.to_dict()
+    errors = validate(user_data)
+    if errors:
+        return render_template(
+            'users/new.html',
+            user=user_data,
+            errors=errors,
+        )
+    repo = UserRepository()
+    repo.save(user_data)
+
+    flash('Пользователь успешно добавлен', 'success')
+    return redirect(url_for('users_get'), code=302)
 
 
-@app.route("/courses/<id>")
-def courses_show(id):
-    return f"Course id: {id}"
+@app.route('/users/new')
+def users_new():
+    user = {'name': '', 'email': ''}
+    errors = {}
+    return render_template(
+        'users/new.html',
+        user=user,
+        errors=errors,
+    )
+
+
+@app.route('/users/<id>/edit')
+def users_edit(id):
+    repo = UserRepository()
+    user = repo.find(id)
+    errors = {}
+
+    return render_template(
+        'users/edit.html',
+        user=user,
+        errors=errors,
+    )
+
+
+@app.route('/users/<id>/patch', methods=['POST'])
+def users_patch(id):
+    repo = UserRepository()
+    user = repo.find(id)
+    data = request.form.to_dict()
+
+    errors = validate(data)
+    if errors:
+        return render_template(
+            'users/edit.html',
+            user=user,
+            errors=errors,
+        ), 422
+
+    user['name'] = data['name']
+    user['email'] = data['email']
+    repo.save(user)
+    flash('Пользователь успешно обновлен', 'success')
+    return redirect(url_for('users_get'))
+
+
+@app.route('/users/<id>')
+def users_show(id):
+    repo = UserRepository()
+    user = repo.find(id)
+    print(user)
+    return render_template(
+        'users/show.html',
+        user=user,
+    )
+
+
+def validate(user):
+    errors = {}
+    if not user['name']:
+        errors['name'] = "Can't be blank"
+    if not user['email']:
+        errors['email'] = "Can't be blank"
+    return errors
